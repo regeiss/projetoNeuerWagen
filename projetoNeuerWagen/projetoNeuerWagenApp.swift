3ddd716d6248260cbd3ba1a4b70b070cf9f479bc
Roberto <roberto.geiss.nh@icloud.com>
2023-05-06 12:40:14 -0300

    Initial Commit

---
+++ projetoNeuerWagen/projetoNeuerWagenApp.swift
@@ -0,0 +1,17 @@
+//
+//  projetoNeuerWagenApp.swift
+//  projetoNeuerWagen
+//
+//  Created by Roberto Edgar Geiss on 06/05/23.
+//
+
+import SwiftUI
+
+@main
+struct projetoNeuerWagenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
