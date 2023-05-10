4585c5a330f4cffc2a832fd011e9fde8a37e8c9b
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-05-10 16:58:32 -0300

    inclusao do odigo

--- ProjetoNeuerWagen/Code/Features/SideMenu/Screens/SideMenuScreen.swift
+++ ProjetoNeuerWagen/Code/Features/SideMenu/Screens/SideMenuScreen.swift
@@ -7,14 +7,87 @@
 
 import SwiftUI
 
-struct SideMenuScreen: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+struct SideMenuView: View
+{
+    @Binding var showMenu: Bool
+    let router = MyRouter.shared
+    let appBuild = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
+    //Text("Versão \(Bundle.versionBuild)").bold().font(.title).foregroundColor(.secondary)
 
-struct SideMenuScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        SideMenuScreen()
+    var body: some View
+    {
+        VStack(alignment: .leading)
+        {
+            HStack
+            {
+                Image(systemName: "rectangle.and.pencil.and.ellipsis")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Login")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 100)
+            .onTapGesture
+            {
+                showMenu = false
+                router.toLogin()
+            }
+            HStack
+            {
+                Image(systemName: "person")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Perfil")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            .onTapGesture
+            {
+                showMenu = false
+                router.toListaPerfil()
+            }
+            HStack
+            {
+                Image(systemName: "envelope")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Mensagens")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            .onTapGesture
+            {
+                showMenu = false
+                router.toMenu()
+            }
+            HStack
+            {
+                Image(systemName: "gearshape")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Configurações")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            .onTapGesture
+            {
+                showMenu = false
+                router.toConfig()
+            }
+            Spacer()
+            HStack
+            {
+                Text("WerkstadtG v.")
+                Text(appBuild)
+                    .foregroundColor(.gray)
+            }.padding([.leading, .bottom])
+        }.padding()
+         .frame(maxWidth: .infinity, alignment: .leading)
+         .background(Color(red: 32/255, green: 32/255, blue: 32/255))
+         .edgesIgnoringSafeArea(.all)
     }
 }
