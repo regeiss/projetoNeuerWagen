3808b537f02c2badbb694192cd285b4439d6a8e9
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-05-11 17:41:49 -0300

    Update WhatsNewScreen.swift

--- ProjetoNeuerWagen/Code/Features/WhatsNew/Screen/WhatsNewScreen.swift
+++ ProjetoNeuerWagen/Code/Features/WhatsNew/Screen/WhatsNewScreen.swift
@@ -7,14 +7,21 @@
 
 import SwiftUI
 
-struct WhatsNewScreen: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct WhatsNewScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        WhatsNewScreen()
+struct WhatsNewScreen: View   
+{
+    @State var showNew: Bool = true
+    @State var align: HorizontalAlignment = .center
+    @State var color: Color = .accentColor
+    @State var size: String = "mini"
+    @State var label: String = "Mostrar notas release"
+    @State var labelImage: String = "arrow.up.circle"
+    @State var labelColor: Color = Color(UIColor.systemBackground)
+    @State var history: Bool = true
+    @State var data: String = "data"
+    @State var showDrop: Bool = false 
+    
+    var body: some View
+    {
+        SwiftNEW(show: $showNew, align: $align, color: $color, size: $size, labelColor: $labelColor, label: $label, labelImage: $labelImage, history: $history, data: .constant("data"), showDrop: $showDrop)
     }
 }
